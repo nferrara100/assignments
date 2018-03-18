47d087a1e9112a12a12fdf31cf9f6a7fa78b988c
Nicholas Ferrara <nferrara100@gmail.com>
2018-03-18 14:38:09 +0000

    ADS Part 1

--- ads/src/CountedElement.java
+++ ads/src/CountedElement.java
@@ -2,24 +2,43 @@ public class CountedElement<E extends Comparable<E>> implements Comparable<Count
 	private E element;
 	private int count;
 
-	public CountedElement(E e, int count){
+	public CountedElement(E e, int count) {
 		//constructor - to complete
+		element = e;
+		this.count = count;
 	}
 	
-	public CountedElement(E e){
+	public CountedElement(E e) {
 		//constructor - to complete
+		this(e, 1);
 	}
-
+	
 	//add getters and setters
+	public void setElement(E element) {
+		this.element = element;
+	}
+	
+	public E getElement () {
+		return element;
+	}
+	
+	public void setCount (int count) {
+		this.count = count;
+	}
+	
+	public int getCount () {
+		return count;
+	}
 	
 	//add toString() method
 	
-	
-
+	public String toString () {
+		return "(" + element.toString() + ", " + count + ")"; //stub to be improved
+	}
 	
 	public int compareTo(CountedElement<E> sC1) {
-    
-		//to complete
+		return element.compareTo(sC1.getElement());
+		
 	}
 
 }
--- ads/src/WordProcessor.java
+++ ads/src/WordProcessor.java
@@ -1,7 +1,8 @@
 
 //import classes for file input - scanner etc.
 import java.io.FileNotFoundException;
-import java.util.Set;
+import java.util.*;
+import java.io.*;
 //import implementing set (eg. TreeSet)
 
 
@@ -12,6 +13,17 @@ public class WordProcessor {
 		//implement this static method to create a
 		// String representation of set - 5 comma separated elements per line
 		// assume that type E has a toString method
+		String returnString = "";
+		int elInLine = 0;
+		for (E el : inputSet) {
+			returnString += el + ", ";
+			elInLine++;
+			if (elInLine >= 5) {
+				elInLine = 0;
+				returnString += System.getProperty("line.separator");
+			}
+		}
+		return returnString;
 	}
 
 	/**
@@ -19,15 +31,63 @@ public class WordProcessor {
 	 */
 	public static void main(String[] args) {
 		//create a set of type String called wordSet
+		TreeSet<String> wordSet = new TreeSet<String>();
 		//create a set of type CountedElement<String> called countedWordSet 
+		TreeSet<CountedElement<String>> countedWordSet = new TreeSet<CountedElement<String>>();
 		
+		final int NUM_ARGS = 3;
 		//for each input file (assume 3 arguments, each the name of a file)
+		for (int i = 0; i < NUM_ARGS; i++) {
+			String fileName = args[i];
+	        String line = null;
+
+	        try {
+	            // FileReader reads text files in the default encoding.
+	            FileReader fileReader = 
+	                new FileReader(fileName);
+
+	            // Always wrap FileReader in BufferedReader.
+	            BufferedReader bufferedReader = 
+	                new BufferedReader(fileReader);
+
+	            while((line = bufferedReader.readLine()) != null) {// remove any quotation marks
+	                //System.out.println(line);
+	                if (!wordSet.contains(line)) {
+	                	wordSet.add(line);
+	                	countedWordSet.add(new CountedElement<String>(line));
+	                }
+	                else {
+	                	Iterator<CountedElement<String>> iterator = countedWordSet.iterator();
+	                    while(iterator.hasNext()) {
+	                    	CountedElement<String> el = iterator.next();
+	                        if(el.getElement().equals(line)) {
+                				el.setCount(el.getCount() + 1);
+                				break;
+	                        }
+	                    }
+	                }
+	            }
+	            
+	            bufferedReader.close();         
+	        }
+	        catch(FileNotFoundException ex) {
+	            System.out.println(
+	                "Unable to open file '" + 
+	                fileName + "'");                
+	        }
+	        catch(IOException ex) {
+	            ex.printStackTrace();
+	        }
+			
+			
+			
 		//  for each word w
 		//     if wordset doesnt contain w:
 		//        add w to wordset
 	    //        add new element to countedWordSet
 		//     else
 		//        increment numeric part of element in countedWordSet containing w
+		}
 		
 
 	System.out.println(displaySet(countedWordSet));
