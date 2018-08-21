da350168af1a58a5a3807e80480f1d952a69cb41
Nicholas Ferrara <nferrara100@gmail.com>
2018-08-21 13:50:06 +0100

    Added last ADS homework

--- ads/src/Part2Test.java
+++ ads/src/Part2Test.java
@@ -4,6 +4,7 @@ public class Part2Test {
 
 	public static void main(String[] args) {
 		BSTBag<String> bag = new BSTBag<String>();
+		System.out.println(bag.isEmpty());
 		
 		bag.add("Hello");
 		bag.add("World");
@@ -15,8 +16,8 @@ public class Part2Test {
 		
 		System.out.println(bag2.equals(bag));
 		
-		bag2.add("Hello");
 		bag2.add("World");
+		bag2.add("Hello");
 		bag2.add("AAA");
 		
 		System.out.println(bag2.equals(bag));
@@ -29,11 +30,68 @@ public class Part2Test {
 		
 		System.out.println("New size: " + bag.size());
 		
+		bag.add("Same");
+		bag.add("Same");
+		bag.add("Nick");
+		bag.remove("Nick");
+		
 		Iterator<String> it = bag.iterator();
+		
+		bag.add("Dev");
+		
+		System.out.println(bag2.contains("Nick"));
+		System.out.println(bag.contains("Nick"));
 		
 		while(it.hasNext()) {
 			System.out.println(it.next());
 		}
+		
+		
+		
+		
 	}
+	
+	
+//	
+//	Iterator<E> it = iterator();
+//	while (it.hasNext()) {
+//		E searchEl = it.next();
+//		Iterator<E> thatIterator = that.iterator();
+//		boolean elEquals = false;
+//		while(thatIterator.hasNext()) {
+//			if (thatIterator.next() == searchEl) {
+//				elEquals = true;//num
+//			}
+//		}
+//		if (elEquals != true) {
+//			return false;
+//		}
+//	}
+//	if (!thatIterator.hasNext()) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+	
+	
+	
+	
+	
+	
+//	
+//	LinkedStack<BSTNode<E>> saved = new LinkedStack<BSTNode<E>>();
+//	
+//	//Clean out removed elements
+//	for (int i = 0; i < currentSize - elementsChecked; i++) {
+//		BSTNode<E> nodeToCheck = track.pop();
+//		if (!nodeToCheck.isRemoved()) {
+//			saved.push(track.pop());
+//		}
+//	}
+//	for (int i = 0; i < currentSize - elementsChecked; i++) {
+//		track.push(saved.pop());
+//	}
+//	return (! track.empty());
 	
 }
